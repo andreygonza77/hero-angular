a331ad4c0ce5ff08e3dc2531647862eba57708b2
andreygonza77 <gonzalesandrey748@gmail.com>
2026-04-21 21:13:46 +0200

    typescript done

--- src/app/components/hero-edit/hero-edit.ts
+++ src/app/components/hero-edit/hero-edit.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../../models/hero.model';
 
 @Component({
-  selector: 'app-hero-edit',
-  imports: [],
+  selector: 'app-card-edit',
+  imports: [Hero, FormsModule],
   templateUrl: './hero-edit.html',
   styleUrl: './hero-edit.css',
 })
-export class HeroEdit {}
+export class HeroEdit {
+  @Input() hero: Hero = {
+    id: -1,
+    nome: "",
+    potere: "",
+    completata: false
+  };
+  @Output() onSave = new EventEmitter<Hero>();
+
+  save() {
+    // this.hero.id = this.hero.id === null ? -1 : this.hero.id;
+    if (typeof this.hero.id === 'undefined' || this.hero.id === null) {
+      this.hero.id = -1;
+    }
+    this.onSave.emit(this.hero);
+    this.reset();
+  }
+
+  reset() {
+    this.hero = {
+      id: -1,
+      nome: "",
+      potere: "",
+      completata: false
+    };
+  }
+}
