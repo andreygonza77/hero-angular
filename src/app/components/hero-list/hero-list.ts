ba20725cc6580df496a48de1bf1bc740468816a2
andreygonza77 <gonzalesandrey748@gmail.com>
2026-04-21 21:12:01 +0200

    class fix

--- src/app/components/hero-list/hero-list.ts
+++ src/app/components/hero-list/hero-list.ts
@@ -1,9 +1,53 @@
 import { Component } from '@angular/core';
+import { Hero } from '../../models/hero.model';
+import { CommonModule } from '@angular/common';
+import { HeroCard } from '../hero-card/hero-card';
 
 @Component({
-  selector: 'app-hero-list',
-  imports: [],
+  selector: 'app-card-list',
+  imports: [HeroCard, CommonModule],
   templateUrl: './hero-list.html',
   styleUrl: './hero-list.css',
 })
-export class HeroList {}
+export class HeroList {
+
+  heroSelected: Hero = {} as Hero;
+
+  heroes: Hero[] = [
+
+    {
+      id: 1,
+      nome: "faliani",
+      potere: "lavarsi le mani",
+      completata: false
+    },
+    {
+      id: 2,
+      nome: "piemontese",
+      potere: "ridere",
+      completata: false
+    }
+
+  ];
+  contatore = 0;
+  markAsDone(hero: Hero) {
+    hero.completata = true;
+    this.contatore++;
+  }
+
+  selectHero(hero: Hero) {
+    this.heroSelected = {...hero};
+  }
+
+  saveHero(hero: Hero) {
+    if (hero.id === -1 || hero.id === null) {
+      hero.id = this.heroes.length + 1;
+      this.heroes.push(hero);
+    } else {
+      const index = this.heroes.findIndex(h => h.id === hero.id);
+      if (index !== -1) {
+        this.heroes[index] = hero;
+      }
+    }
+  }
+}
