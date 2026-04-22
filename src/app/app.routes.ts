233c2973bc9a79d6b15da7cac62378f6999839e3
andreygonza77 <gonzaleandrey748@gmail.com>
2026-04-22 13:46:44 +0200

    inizio rotte

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeroCard } from './components/hero-card/hero-card';
+import { HeroList } from './components/hero-list/hero-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeroCard },
+    { path: 'hero/:id', component: HeroCard},
+    { path: 'hero/list', component: HeroList},
+    { path: '**', redirectTo: '' }];
+
