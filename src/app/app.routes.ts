604429d369155a2a5c8c601eaff1d4bf34418bbf
andreygonza77 <gonzalesandrey748@gmail.com>
2026-04-24 21:58:50 +0200

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroList } from './components/hero-list/hero-list';
+import { HeroCard } from './components/hero-card/hero-card';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HeroList },
+    { path: 'hero/:id', component: HeroCard },
+    { path: '**', redirectTo: '' }
+];
