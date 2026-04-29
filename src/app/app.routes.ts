49cffc3de9fe4c81e692dfe51750a9644de6a49c
andreygonza77 <gonzalesandrey748@gmail.com>
2026-04-29 13:43:21 +0200

    cleaned code + missing crud

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HeroList } from './components/hero-list/hero-list';
 import { HeroEdit } from './components/hero-edit/hero-edit';
-import { HeroCard } from './components/hero-card/hero-card';
 
 export const routes: Routes = [
     { path: '', component: HeroList },
     { path: 'hero', component: HeroEdit },
-    { path: 'hero/:id', component: HeroCard },
     { path: '**', redirectTo: '' }
 ];
